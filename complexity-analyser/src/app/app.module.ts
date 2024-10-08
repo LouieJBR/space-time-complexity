488a2f4730bed9887ab0f2c9ccfb4b615d9b5b28
Louie Brown <78971075+LouieJBR@users.noreply.github.com>
2024-10-08 13:15:38 +0100

    Initial Configuration - Initialising angular project - Inistalising backend with Node

---
+++ complexity-analyser/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CodeAnalyzerComponent } from './code-analyzer/code-analyzer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CodeAnalyzerComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
